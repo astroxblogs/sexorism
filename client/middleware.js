45a4b1128050aeafa8cff39edfe322dbb4cd775a
astroxblogs <blogs@astroxhub.com>
2025-09-18 23:00:25 +0530

    feat: Add Prerender.io middleware for Vercel

---
+++ client/middleware.js
@@ -0,0 +1,4 @@
+    // This is the official middleware from Prerender.io
+    // It will automatically handle bot detection and proxying.
+    export { handle } from '@prerender.io/vercel-middleware';
+    
