eebb4c0634fa56015de0fe823fe8da01090dfc75
astroxblogs <blogs@astroxhub.com>
2025-10-06 15:36:32 +0530

    fix: Add .next folder to .gitignore to reduce repository size

--- client/.next/static/chunks/app/admin/settings/page-38a42113754fb834.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6140],{4879:function(n,e,u){Promise.resolve().then(u.bind(u,2786))}},function(n){n.O(0,[3464,2786,2971,2117,1744],function(){return n(n.s=4879)}),_N_E=n.O()}]);
